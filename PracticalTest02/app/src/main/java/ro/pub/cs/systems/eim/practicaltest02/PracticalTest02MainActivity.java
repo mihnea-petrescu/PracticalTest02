b3d6b62004f054208421ed1ec7dbeed56a1a9526
Root USO User <student@stud.acs.upb.ro>
2019-05-23 11:27:59 +0300

    done client-server

---
+++ PracticalTest02/app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/PracticalTest02MainActivity.java
@@ -0,0 +1,82 @@
+package ro.pub.cs.systems.eim.practicaltest02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class PracticalTest02MainActivity​ extends AppCompatActivity {
+    Button startServer;
+    Button sendCommand;
+
+    EditText serverPort;
+    EditText clientCommand;
+    EditText invisibleTextView;
+
+    ServerThread serverThread;
+    ClientThread clientThread;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practical_test02_main);
+
+        startServer = findViewById(R.id.startServer);
+        sendCommand = findViewById(R.id.sendClient);
+
+        serverPort = findViewById(R.id.portServer);
+        clientCommand = findViewById(R.id.commandClient);
+        invisibleTextView = findViewById(R.id.invizibil);
+
+        startServer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String port = serverPort.getText().toString();
+                Log.v("dbg", port);
+                serverThread = new ServerThread(Integer.parseInt(port));
+                serverThread.start();
+            }
+        });
+
+        sendCommand.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String command = clientCommand.getText().toString();
+                String port = serverPort.getText().toString();
+
+                clientThread = new ClientThread(command, Integer.parseInt(port), invisibleTextView);
+                clientThread.start();
+            }
+        });
+
+        invisibleTextView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Toast.makeText(PracticalTest02MainActivity​.this, invisibleTextView.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (serverThread != null) {
+            serverThread.stopThread();
+        }
+        super.onDestroy();
+    }
+}
